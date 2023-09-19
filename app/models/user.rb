b292302ce69e46dde91b1263abe4378b794b2ae3
Ajish-TM <ajish.tm427@gmail.com>
2023-09-19 13:11:47 +0530

    till video no.223 message table aND ADD HAS_Many message and belongs to user for message table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
           presence: true,
           length: { minimum: 3, maximum: 15 },
           uniqueness: true
-
+    has_many :messages
     has_secure_password     
 end
